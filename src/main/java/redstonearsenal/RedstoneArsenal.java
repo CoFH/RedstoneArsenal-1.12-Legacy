749ba880cc03244d82a926e0174ea7685c7b6981
King Lemming <kinglemming@gmail.com>
2014-07-31 14:26:37 -0400

    Updated version. Yay.
    Signed-off-by: King Lemming <kinglemming@gmail.com>

--- src/main/java/redstonearsenal/RedstoneArsenal.java
+++ src/main/java/redstonearsenal/RedstoneArsenal.java
@@ -28,7 +28,7 @@ public class RedstoneArsenal extends BaseMod {
 
 	public static final String modId = "RedstoneArsenal";
 	public static final String modName = "Redstone Arsenal";
-	public static final String version = "1.7.10R1.0.1B1";
+	public static final String version = "1.7.10R1.0.1B2";
 	public static final String dependencies = "required-after:CoFHCore@[" + CoFHCore.version + ",);after:ThermalExpansion";
 	public static final String releaseURL = "http://github.com/CoFH/RedstoneArsenal/blob/master/VERSION";
 
