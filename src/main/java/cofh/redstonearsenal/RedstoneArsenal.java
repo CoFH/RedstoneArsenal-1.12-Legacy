220ddbfe83d6e461d8984b59c7c09ffd9135e169
King Lemming <kinglemming@gmail.com>
2017-03-04 14:45:59 -0500

    Added update URL.
    Signed-off-by: King Lemming <kinglemming@gmail.com>

--- src/main/java/cofh/redstonearsenal/RedstoneArsenal.java
+++ src/main/java/cofh/redstonearsenal/RedstoneArsenal.java
@@ -22,15 +22,16 @@ import org.apache.logging.log4j.Logger;
 
 import java.io.File;
 
-@Mod (modid = RedstoneArsenal.MOD_ID, name = RedstoneArsenal.MOD_NAME, version = RedstoneArsenal.VERSION, dependencies = RedstoneArsenal.DEPENDENCIES, guiFactory = RedstoneArsenal.MOD_GUI_FACTORY)
+@Mod (modid = RedstoneArsenal.MOD_ID, name = RedstoneArsenal.MOD_NAME, version = RedstoneArsenal.VERSION, dependencies = RedstoneArsenal.DEPENDENCIES, updateJSON = RedstoneArsenal.UPDATE_URL)
 public class RedstoneArsenal {
 
 	public static final String MOD_ID = "redstonearsenal";
 	public static final String MOD_NAME = "Redstone Arsenal";
 
-	public static final String VERSION = "1.2.0";
-	public static final String VERSION_MAX = "1.3.0";
+	public static final String VERSION = "2.0.0";
+	public static final String VERSION_MAX = "2.1.0";
 	public static final String VERSION_GROUP = "required-after:" + MOD_ID + "@[" + VERSION + "," + VERSION_MAX + ");";
+	public static final String UPDATE_URL = "https://raw.github.com/cofh/version/master/" + MOD_ID + "_update.json";
 
 	public static final String DEPENDENCIES = CoFHCore.VERSION_GROUP + ThermalFoundation.VERSION_GROUP + "after:ThermalExpansion";
 	public static final String MOD_GUI_FACTORY = "cofh.redstonearsenal.gui.GuiConfigRAFactory";
