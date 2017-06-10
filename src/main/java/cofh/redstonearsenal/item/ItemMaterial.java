19f40895fb12ea4bf5a8af5dfa0932bde800c761
King Lemming <kinglemming@gmail.com>
2017-06-10 08:25:51 -0400

    Ensure TE recipes are correctly registered.
    Signed-off-by: King Lemming <kinglemming@gmail.com>

--- src/main/java/cofh/redstonearsenal/item/ItemMaterial.java
+++ src/main/java/cofh/redstonearsenal/item/ItemMaterial.java
@@ -1,11 +1,15 @@
 package cofh.redstonearsenal.item;
 
+import cofh.api.util.ThermalExpansionHelper;
 import cofh.core.item.ItemMulti;
 import cofh.core.util.core.IInitializer;
 import cofh.redstonearsenal.RedstoneArsenal;
+import cofh.thermalfoundation.init.TFFluids;
+import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
 import net.minecraft.item.EnumRarity;
 import net.minecraft.item.ItemStack;
+import net.minecraftforge.fluids.FluidStack;
 import net.minecraftforge.fml.common.Loader;
 
 import static cofh.lib.util.helpers.ItemHelper.*;
@@ -63,6 +67,10 @@ public class ItemMaterial extends ItemMulti implements IInitializer {
 			addRecipe(ShapelessRecipe(dustElectrumFlux, "dustElectrum", "dustRedstone", "dustRedstone", "dustRedstone", "dustRedstone", "dustRedstone"));
 			addRecipe(ShapelessRecipe(gemCrystalFlux, "gemDiamond", "dustRedstone", "dustRedstone", "dustRedstone", "dustRedstone", "dustRedstone"));
 			addSmelting(ingotElectrumFlux, dustElectrumFlux, 0.0F);
+		} else {
+			ThermalExpansionHelper.addSmelterRecipe(4000, dustElectrumFlux, new ItemStack(Blocks.SAND), ingotElectrumFlux);
+			ThermalExpansionHelper.addTransposerFill(4000, cofh.thermalfoundation.item.ItemMaterial.dustElectrum, dustElectrumFlux, new FluidStack(TFFluids.fluidRedstone, 500), false);
+			ThermalExpansionHelper.addTransposerFill(4000, new ItemStack(Items.DIAMOND), gemCrystalFlux, new FluidStack(TFFluids.fluidRedstone, 500), false);
 		}
 		return true;
 	}
