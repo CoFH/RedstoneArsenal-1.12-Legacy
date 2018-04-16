5e2ca3df266d5f67ff7c79cff0a87f844f1ff854
King Lemming <kinglemming@gmail.com>
2018-04-15 23:00:11 -0400

    Carry up changes from ItemMulti.
    Signed-off-by: King Lemming <kinglemming@gmail.com>

--- src/main/java/cofh/redstonearsenal/item/ItemMaterial.java
+++ src/main/java/cofh/redstonearsenal/item/ItemMaterial.java
@@ -22,6 +22,7 @@ public class ItemMaterial extends ItemMulti implements IInitializer {
 
 		super("redstonearsenal");
 
+		register("material");
 		setUnlocalizedName("material");
 		setCreativeTab(RedstoneArsenal.tabItems);
 	}
